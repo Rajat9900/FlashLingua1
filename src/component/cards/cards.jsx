2b019c68e37e6df3aaaa4a92b6ac4a858c352002
Banish <banish18chandel@gmail.com>
2024-08-25 13:37:52 +0530

    Card file changes

--- src/component/cards/cards.jsx
+++ src/component/cards/cards.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 // import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { getallCards, getCard, getFilteredCards } from "../../../services";
 import './ImagesCSS.css';
@@ -48,7 +48,55 @@ const Cards = () => {
   const [nextcard, setNextcard] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [fileurl, setFileurl] = useState('https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3');
+   const [currentAudioUrl, setCurrentAudioUrl] = useState(null);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [sourceFileurl, setSourceFileurl] = useState('https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3');
+
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [audioLoaded, setAudioLoaded] = useState(false);
+  const [hasPlayedOnLoad, setHasPlayedOnLoad] = useState(false);
+   const [playAudio, setPlayAudio] = useState(false);
+  const audioRef = useRef(new Audio());
+  const audioElement = audioRef.current;
+
+  useEffect(() => {
+    // Clean up when component unmounts
+    return () => {
+      audioElement.pause();
+      audioElement.src = '';
+      audioElement.removeEventListener('canplaythrough', handleLoadedMetadata);
+    };
+  }, [audioElement]);
+
+  const handleLoadedMetadata = useCallback(() => {
+    setIsAudioPlaying(true);
+    audioElement.play().catch((error) => {
+      console.error('Error playing audio:', error);
+    });
+  }, [audioElement]);
+
+  const setAudioUrl = useCallback(
+    (audioUrl) => {
+      setCurrentAudioUrl(audioUrl);
+      audioElement.src = audioUrl;
+      audioElement.currentTime = 0;
+      audioElement.addEventListener('canplaythrough', handleLoadedMetadata, { once: true });
+    },
+    [audioElement, handleLoadedMetadata]
+  );
+
+  const playAudioFunc = useCallback(() => {
+    if (audioElement && items.length > 0) {
+      const { sourceAudio,targetAudio } = items[currentCardIndex] || {};
+
+      if (targetAudio && audioLoaded) {
+        setAudioUrl(targetAudio);
+        setHasPlayedOnLoad(true);
+      } else {
+        console.error('Audio URL is undefined or not loaded for the current card.');
+      }
+    }
+  }, [audioElement, items, currentCardIndex, audioLoaded, setAudioUrl]);
 
 
   const [isFlipped, setFlipped] = useState('');
@@ -154,10 +202,17 @@ const Cards = () => {
     
   }
 
+  const handlePlayButtonClick = () => {
+    if (items.length > 0 && items[showcard].targetAudio) {
+      setPlayAudio(true);
+      setAudioUrl(items[showcard].targetAudio);
+    }
+  };
+
   var UniqeKeyVarible = 1;
   return (
     <div className="flex justify-center ">
-
+    <div className="justify-center ">
           {Object.keys(items).length == 0 && <div className="items-center mt-5"><h2>No Records</h2></div>}
   
             {items.map((item, index) => (
@@ -168,7 +223,12 @@ const Cards = () => {
                 <p className="p-2">{item.sourceText}</p>
                  {sourceFileurl != null &&
                   <div className="flex flex-col gap-1 w-[100%] items-center m-4">
-                    <Waveform url={item.sourceAudio} />
+                    {item.sourceAudio != null   && (
+                      <audio key={item.sourceAudio} controls={true}>
+                        <source src={item.sourceAudio} type="audio/ogg" />
+                        Your browser does not support the audio element.
+                      </audio>
+                    )}
                     </div>
                 }
 
@@ -181,17 +241,26 @@ const Cards = () => {
                 <h3 className="m-2 greencolor">In {item.targetLang}</h3>
                 <p className="p-2"> {item.targetText}</p>
 
-                {fileurl != null &&
+                
                   <div className="flex flex-col gap-1 w-[100%] items-center m-4">
-                    <Wavesourceform url={item.targetAudio} />
+                   
+                    
+                      <audio key={item.targetAudio} controls={true} preload="auto" autoPlay>
+                        <source src={item.targetAudio} type="audio/ogg" />
+                        Your browser does not support the audio element.
+                      </audio>
+                    
                     </div>
-                }
+               
 
                
 
                 <div className="flex flex-col gap-1 w-[100%] items-center mb-2 mt-2">
 
                     <div className="w-[100%] flex justify-evenly gap-3 mt-3">
+                   
+
+
                       <button onClick={() => showCard(items[index-1]._id,index-1,0)} disabled={items[index-1] == undefined} className={"hover:bg-[#4CAF50] hover:text-white w-full flex pt-2 pb-2 rounded-xl border-[#E6E6E6] border-2 hover:border-none gap-2 justify-center " + (items[index-1] == undefined ? 'd-none' : '')}>
                         <span className="mt-1">
                           <HiOutlineArrowNarrowLeft />
@@ -232,6 +301,7 @@ const Cards = () => {
             )}
           
      {isLoading &&  <Loader /> }
+    </div>
     </div>
   );
 };
